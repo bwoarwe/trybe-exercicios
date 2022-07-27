62a69bfff833deabff637f905c302bd32bdf8c91
Yago Carvalho <Yago.vasconcelos@hotmail.com>
2022-07-27 20:53:56 -0300

    exercicio final resolvido

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-2-script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-2-script.js
@@ -24,5 +24,26 @@ changeSquareToWhite()
 
 
 //  Crie e execute uma função que corrija o texto da tag <h1>.
+
+function corrigir () {
+    let java = document.getElementsByClassName('title')[0];
+    java.innerHTML = 'Exercício 5.1 - JavaScript'; 
+}
+corrigir()
+
 //  Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
+
+function strong () {
+    let upper = document.querySelectorAll('p')[0]; 
+    upper.innerHTML = upper.innerHTML.toUpperCase();
+}
+strong()
+
 //  Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+
+function showConsole () {
+    let log = document.getElementsByTagName('p');
+    for (i = 0; i < log.length; i++)
+    console.log(log[i].innerHTML); 
+}
+showConsole()
